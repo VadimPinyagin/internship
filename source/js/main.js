67ba80ddd20ad30b4d422ce649d60bc761ae49e9
Vadim <vadimbox85@gmail.com>
2025-12-20 00:04:01 +0300

    Проверяет валидаторами, добавляет изображения для свайпера в шапке. Устанавливает swiper через npm

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,19 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
-
+import Swiper from 'swiper';
+import {Pagination} from 'swiper/modules';
+import 'swiper/css';
 import {menuOpenClose} from './open-close-nav.js';
 
+const initSwiperHero = () => {
+  new Swiper('.hero', {
+    modules: [Pagination],
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+};
+
 menuOpenClose();
+initSwiperHero();
