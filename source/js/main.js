94e7b5a1e5dc0f0998593e5a200d5f3a1692a3d7
Vadim <vadimbox85@gmail.com>
2025-12-23 17:43:03 +0300

    Меняет ссылку на кнопку и все сопуствующие стили

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,5 @@
-import { removeButtonHref } from './utils.js';
 import {toggleMenu} from './open-close-nav.js';
 import { initSwiperHero } from './defines-swiper.js';
 
-removeButtonHref();
 toggleMenu();
 initSwiperHero();
--- source/js/utils.js
+++
@@ -1,7 +0,0 @@
-
-export const removeButtonHref = () => {
-  const disabledLinks = document.querySelectorAll('.button__is--disabled');
-  disabledLinks.forEach((link) => {
-    link.removeAttribute('href');
-  });
-};
