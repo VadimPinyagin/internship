e10c9da2195f5ace989d8317be57992545b958aa
Vadim <vadimbox85@gmail.com>
2025-12-25 17:03:36 +0300

    Добавляет program swiper и стили слайдов и кнопок

--- source/js/defines-swiper.js
+++ source/js/defines-swiper.js
@@ -1,17 +1,51 @@
 import Swiper from 'swiper';
-import {Pagination} from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import {Pagination, Navigation, Scrollbar} from 'swiper/modules';
 
 export const initSwiperHero = () => {
   new Swiper('.hero', {
     modules: [Pagination],
     direction: 'horizontal',
+    0: {
+      loop: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+    },
     loop: true,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
+    },
+  });
+};
+
+export const initSwiperPrograms = () => {
+  new Swiper('.programs__swiper', {
+    modules: [Scrollbar, Navigation],
+    direction: 'horizontal',
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    breakpoints: {
+      0: {
+        loop: false,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        spaceBetween: 15,
+      },
+      768: {
+        scrollbar: {
+          el:'.swiper-scrollbar',
+        },
+        centeredSlides: false,
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1440: {
+        centeredSlides: false,
+        slidesPerView: 3,
+        spaceBetween: 32,
+      },
     },
   });
 };
--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,10 @@
 import {toggleMenu} from './open-close-nav.js';
 import { initSwiperHero } from './defines-swiper.js';
+import { initSwiperPrograms } from './defines-swiper.js';
+
 
 toggleMenu();
 initSwiperHero();
+initSwiperPrograms();
 
 
