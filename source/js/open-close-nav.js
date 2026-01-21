538c9d9da8de0b63e3823c99c5b5661996961e02
VadimPinyagin <vadimbox85@gmail.com>
2026-01-21 14:30:23 +0300

    Дополняет функцию управлеиня меню. При загружке DOM меню закрыто

--- source/js/open-close-nav.js
+++ source/js/open-close-nav.js
@@ -1,17 +1,4 @@
-// const mainNav = document.querySelector('.header__nav--main');
-// const headerButton = document.querySelector('.button--header');
 const headerLogoOverlay = document.querySelector('.header__logo-overlay');
-// mainNav.classList.add('header__nav--is-close');
-
-// const closeMenu = () => {
-//   mainNav.classList.remove('header__nav--is-open');
-//   mainNav.classList.add('header__nav--is-close');
-//   headerButton.classList.remove('button--menu-opened');
-//   document.body.classList.remove('page__body--menu-open');
-//   document.body.style.overflow = '';
-//   document.body.style.marginRight = '';
-// };
-
 const buttonSubMenu = document.querySelectorAll('.header__nav-button');
 const subMenu = document.querySelectorAll('.header__nav-list');
 const anchorLinks = document.querySelectorAll('.menu-link');
@@ -24,6 +11,10 @@ const findAnElement = (elements) =>
 const toggleSubMenu = () => {
   const currentButtons = findAnElement(buttonSubMenu);
   const currentSubMenus = findAnElement(subMenu);
+
+  document.addEventListener('DOMContentLoaded', () => {
+  currentSubMenus.forEach(item => item.value.classList.add('header__nav-list--is-hidden'));
+});
 
   currentButtons.forEach(item => {
     const button = item.value;
@@ -87,80 +78,3 @@ const toggleSubMenu = () => {
 };
 
 toggleSubMenu();
-
-
-// let currentButton;
-// let currentSubMenu;
-
-// buttonSubMenu.forEach((value, index) => {
-//   if(index === 0){
-//     currentButton = value;
-//     console.log(currentButton);
-//   }
-// });
-
-// subMenu.forEach((value, index) => {
-//   if(index === 0){
-//     currentSubMenu = value;
-//     console.log(currentSubMenu);
-//   }
-// });
-
-
-// button.addEventListener('click', (e) => {
-//   button.classList.toggle('active')
-
-//   if (button.classList.contains('active')) {
-//     button.setAttribute('aria-expanded', 'true')
-//     menu.setAttribute('aria-hidden', 'false')
-//     menuLinks.forEach(link => link.setAttribute('tabindex', '0'))
-//   } else {
-//     button.setAttribute('aria-expanded', 'false')
-//     menu.setAttribute('aria-hidden', 'true')
-//     menuLinks.forEach(link => link.setAttribute('tabindex', '-1'))
-//   }
-// })
-
-// const toggleMenu = () => {
-//   if (headerButton) {
-//     headerButton.addEventListener('click', () => {
-//       const isOpen = mainNav.classList.toggle('header__nav--is-open');
-//       headerLogo.classList.add('header__logo--overlay');
-//       mainNav.classList.toggle('header__nav--is-close', !isOpen);
-//       headerButton.classList.toggle('button--menu-opened');
-
-//       const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
-
-//       if (isOpen) {
-//         document.body.style.overflow = 'hidden';
-//         document.body.style.marginRight = `${scrollbarWidth}px`;
-//       } else {
-//         document.body.style.overflow = '';
-//         document.body.style.marginRight = '';
-//         headerLogo.classList.remove('header__logo--overlay');
-//       }
-//       document.body.classList.toggle('page__body--menu-open', isOpen);
-//     });
-//     const handleEscapeKey = (event) => {
-//       if (event.key === 'Escape') {
-//         closeMenu();
-//       }
-//     };
-//     const handleClickOutside = (event) => {
-//       const isClickInside = mainNav.contains(event.target) || headerButton.contains(event.target);
-//       if (!isClickInside && mainNav.classList.contains('header__nav--is-open')) {
-//         closeMenu();
-//       }
-//     };
-//     const handleAnchorClick = () => {
-//       closeMenu();
-//     };
-//     const anchorLinks = mainNav.querySelectorAll('a[href^="#"]');
-//     anchorLinks.forEach((link) => link.addEventListener('click', handleAnchorClick));
-
-//     document.addEventListener('click', handleClickOutside);
-//     document.addEventListener('keydown', handleEscapeKey);
-//   }
-// };
-
-// toggleMenu();
