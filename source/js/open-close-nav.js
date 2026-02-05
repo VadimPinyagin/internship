e81e07e6483502003e6fcc4f76d598e5eafbf442
VadimPinyagin <vadimbox85@gmail.com>
2026-02-05 07:26:06 +0300

    Делает навигацию в header блоком

--- source/js/open-close-nav.js
+++ source/js/open-close-nav.js
@@ -1,6 +1,6 @@
-const nav = document.querySelector('.header__nav');
+const nav = document.querySelector('.nav');
 const btnBurger = nav.querySelector('.btn--burger');
-const subButtons = nav.querySelectorAll('.header__nav-btn--submenu');
+const subButtons = nav.querySelectorAll('.nav-btn--submenu');
 const anchorLinks = nav.querySelectorAll('a[href^="#"]');
 const hiddenClass = 'is-hidden';
 const isClose = nav.classList.contains(hiddenClass);
@@ -8,7 +8,7 @@ const isClose = nav.classList.contains(hiddenClass);
 const closeMenu = () => {
   nav.classList.toggle(hiddenClass);
   btnBurger.classList.remove('btn--menu-opened');
-  nav.classList.remove('header__nav--overlay');
+  nav.classList.remove('nav--overlay');
 };
 
 anchorLinks.forEach((link) => link.addEventListener('click', closeMenu));
@@ -30,26 +30,26 @@ const toggleMenu = () => {
     if (e.target === btnBurger) {
       nav.classList.toggle(hiddenClass);
       btnBurger.classList.toggle('btn--menu-opened');
-      nav.classList.toggle('header__nav--overlay');
+      nav.classList.toggle('nav--overlay');
       if (isClose) {
         subButtons.forEach((button) => {
           const subMenuContainer = button.parentElement;
           subMenuContainer.classList.add(hiddenClass);
-          button.classList.remove('header__nav--btn-submenu-open');
+          button.classList.remove('nav--btn-submenu-open');
         });
       }
     }
 
-    if (e.target.classList.contains('header__nav-btn--submenu')) {
+    if (e.target.classList.contains('nav-btn--submenu')) {
       const subButton = e.target;
       const subMenuContainer = subButton.parentElement;
 
       if (!isClose) {
         subMenuContainer.classList.add(hiddenClass);
-        subButton.classList.remove('header__nav--btn-submenu-open');
+        subButton.classList.remove('nav--btn-submenu-open');
       } else {
         subMenuContainer.classList.toggle(hiddenClass);
-        subButton.classList.toggle('header__nav--btn-submenu-open');
+        subButton.classList.toggle('nav--btn-submenu-open');
       }
     }
   });
